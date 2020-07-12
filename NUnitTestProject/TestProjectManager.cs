99793bddf2ed0a399b53a2412e9a1f2c627b3860
Vanada1 <vovashvoev122300@gmail.com>
2020-07-12 19:47:55 +0700

    test(TestProjectManager.cs): создан файл по тестеровке ProjectManager

---
+++ NUnitTestProject/TestProjectManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using NUnit.Framework;
+using ContactsApp;
+using NUnit.Framework.Internal;
+
+namespace NUnitTestProject
+{
+	[TestFixture]
+	public class TestProjectManager
+	{
+		
+	}
+}
